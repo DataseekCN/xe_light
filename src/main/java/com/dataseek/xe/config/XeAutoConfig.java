0f73f95e443badf9bf02c6bb164cc56b6638de3a
samtao821030 <samtao821030@gmail.com>
2019-08-13 17:02:39 +0800

    initial new project2

--- src/main/java/com/dataseek/xe/config/XeAutoConfig.java
+++ src/main/java/com/dataseek/xe/config/XeAutoConfig.java
@@ -26,8 +26,10 @@ package com.dataseek.xe.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @org.springframework.context.annotation.Configuration
 @EnableConfigurationProperties(XeProperties.class)
@@ -43,8 +45,18 @@ public class XeAutoConfig {
         basicDataSource.setUsername(xeProperties.getDb_username());
         basicDataSource.setPassword(xeProperties.getDb_password());
         basicDataSource.setInitialSize(xeProperties.getInitialSize());
-
+        basicDataSource.setMaxActive(xeProperties.getMaxActive());
+        basicDataSource.setMinIdle(xeProperties.getMinIdle());
+        basicDataSource.setValidationQuery("select now() ");
+        basicDataSource.setTestOnBorrow(true);
         basicDataSource.setDefaultAutoCommit(false);
         return basicDataSource;
+    }
+
+    @Bean(name="xeJdbcTemplate")
+    public JdbcTemplate initJdbcTemplate(@Qualifier("xeDataSource")DruidDataSource xeDataSource){
+        JdbcTemplate xeJdbcTemplate = new JdbcTemplate();
+        xeJdbcTemplate.setDataSource(xeDataSource);
+        return xeJdbcTemplate;
     }
 }
