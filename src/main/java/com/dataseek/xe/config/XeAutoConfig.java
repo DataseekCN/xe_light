a0334722e97aa431b3cd290cfe19c5a4ac4fda47
samtao821030 <samtao821030@gmail.com>
2019-08-13 15:59:18 +0800

    initial new project

---
+++ src/main/java/com/dataseek/xe/config/XeAutoConfig.java
@@ -0,0 +1,28 @@
+/**
+ * ━━━━━━神兽出没━━━━━━
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑, 永无BUG!
+ * 　　　　 ┃　　　┃Code is far away from bug with the animal protecting
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━感觉萌萌哒━━━━━━
+ * Module Desc:
+ * User: taosm
+ * DateTime: 2019-08-13 15
+ */
+package com.dataseek.xe.config;
+
+public class XeAutoConfig {
+}
