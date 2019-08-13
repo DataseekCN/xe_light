340d570320e2f1d7b9003460af3649e9299eda4d
samtao821030 <samtao821030@gmail.com>
2019-08-13 16:49:42 +0800

    initial new project2

--- src/main/java/com/dataseek/xe/MainApplication.java
+++ src/main/java/com/dataseek/xe/MainApplication.java
@@ -1,13 +1,16 @@
 package com.dataseek.xe;
 
+import com.dataseek.xe.config.XeAutoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SuppressWarnings("SpringBootApplicationSetup")
 @SpringBootApplication
+@Import({XeAutoConfig.class})
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.dataseek"})
 public class MainApplication {
--- src/main/java/com/dataseek/xe/config/XeAutoConfig.java
+++ src/main/java/com/dataseek/xe/config/XeAutoConfig.java
@@ -24,5 +24,27 @@
  */
 package com.dataseek.xe.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@org.springframework.context.annotation.Configuration
+@EnableConfigurationProperties(XeProperties.class)
 public class XeAutoConfig {
+    @Autowired
+    private XeProperties xeProperties;
+
+    @Bean(name="xeDataSource")
+    public DruidDataSource initBasicDataSource(){
+        DruidDataSource basicDataSource = new DruidDataSource();
+        basicDataSource.setDriverClassName(xeProperties.getDriver());
+        basicDataSource.setUrl(xeProperties.getJdbc_url());
+        basicDataSource.setUsername(xeProperties.getDb_username());
+        basicDataSource.setPassword(xeProperties.getDb_password());
+        basicDataSource.setInitialSize(xeProperties.getInitialSize());
+
+        basicDataSource.setDefaultAutoCommit(false);
+        return basicDataSource;
+    }
 }
