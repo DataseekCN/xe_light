be749e457097d34d02aa61ed55bf724fc1549c44
vandergang <wg360@jsti.com>
2019-08-20 16:22:46 +0800

    mod

--- src/main/java/com/dataseek/xe/controller/UserController.java
+++ src/main/java/com/dataseek/xe/controller/UserController.java
@@ -7,6 +7,8 @@ import com.dataseek.xe.service.IUserService;
 import com.dataseek.xe.util.DataUtil;
 import com.dataseek.xe.util.ResponseDto;
 import com.dataseek.xe.util.XeConsts;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,33 +19,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
+    protected static Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private IUserService userService;
 
-
     @RequestMapping("/signup")
     public ResponseDto signup(@RequestBody JSONObject json) {
-        UserInfo userDto = new UserInfo();
-        userDto.setEmail(json.getString("email"));
-        userDto.setFirstName(json.getString("firstName"));
-        userDto.setLastName(json.getString("lastName"));
-        userDto.setPassword(json.getString("password"));
-        userDto.setActive("N");
-        String userId = DataUtil.getUserIdByAtomic();
-        userDto.setUserId(userId);
-
         ResponseDto responseDto = new ResponseDto();
-        //校验
-        if (!DataUtil.checkEmail(userDto.getEmail())) {
+        try {
+            UserInfo userDto = new UserInfo();
+            userDto.setEmail(json.getString("email"));
+            userDto.setFirstName(json.getString("firstName"));
+            userDto.setLastName(json.getString("lastName"));
+            //MD5加密
+            String mdStr = DataUtil.EncoderByMd5(json.getString("password"));
+            userDto.setPassword(mdStr);
+            userDto.setActive("N");
+            String userId = DataUtil.getUserIdByAtomic();
+            userDto.setUserId(userId);
+
+            //校验
+            if (!DataUtil.checkEmail(userDto.getEmail())) {
+                responseDto.setStatus(XeConsts.RESPONSE_STATUS_FAILURE);
+                responseDto.setError_message("email format error.");
+                return  responseDto;
+            }
+            userService.insertUser(userDto);
+
+            responseDto.setStatus(XeConsts.RESPONSE_STATUS_SUCCESS);
+            responseDto.setUser_id(userId);
+            return responseDto;
+        }
+        catch (Exception ex) {
+            logger.error(ex.toString(), ex);
             responseDto.setStatus(XeConsts.RESPONSE_STATUS_FAILURE);
-            responseDto.setError_message("email format error.");
+            responseDto.setError_message("signup error.");
             return  responseDto;
         }
-        userService.insertUser(userDto);
-
-        responseDto.setStatus(XeConsts.RESPONSE_STATUS_SUCCESS);
-        responseDto.setUser_id(userId);
-        return responseDto;
     }
 }
--- src/main/java/com/dataseek/xe/util/DataUtil.java
+++ src/main/java/com/dataseek/xe/util/DataUtil.java
@@ -1,5 +1,10 @@
 package com.dataseek.xe.util;
 
+import sun.misc.BASE64Encoder;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -48,6 +53,21 @@ public class DataUtil {
         int cnt = atomicInteger.get();
         String date = simpleDateFormat.format(new Date());
         return date + cnt;
+    }
+
+    /**利用MD5进行加密
+     * @param str  待加密的字符串
+     * @return  加密后的字符串
+     * @throws NoSuchAlgorithmException  没有这种产生消息摘要的算法
+     * @throws UnsupportedEncodingException
+     */
+    public static String EncoderByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        //确定计算方法
+        MessageDigest md5=MessageDigest.getInstance("MD5");
+        BASE64Encoder base64en = new BASE64Encoder();
+        //加密后的字符串
+        String newstr=base64en.encode(md5.digest(str.getBytes("utf-8")));
+        return newstr;
     }
 
 }
