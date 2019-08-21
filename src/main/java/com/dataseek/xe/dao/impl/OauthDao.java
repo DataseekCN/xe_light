c518528c8028e00aac0b8a5c6d215d29774353a4
samtao821030 <samtao821030@gmail.com>
2019-08-21 19:24:08 +0800

    实现部分oauth逻辑

--- src/main/java/com/dataseek/xe/dao/impl/OauthDao.java
+++ src/main/java/com/dataseek/xe/dao/impl/OauthDao.java
@@ -25,9 +25,23 @@
 package com.dataseek.xe.dao.impl;
 
 import com.dataseek.xe.dao.IOauthDao;
+import com.dataseek.xe.entity.OauthInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class OauthDao implements IOauthDao {
-    //根据
+    @Autowired
+    private JdbcTemplate xeJdbcTemplate;
+
+    //根据app账户名和etsy账户名查询etsy token管理记录
+    public OauthInfo queryTokenRecordByAppEtsyAccount(String app_account,String etsy_account){
+        OauthInfo oauthInfo = new OauthInfo();
+        String query_sql = "select admin_id,app_account,etsy_account,request_token," +
+                "request_secret,access_token,access_secret,update_time " +
+                "from xero.etsy_token_admin where app_account=? and etsy_account=? ";
+        Object[] params = new Object[]{app_account,etsy_account};
+        return oauthInfo;
+    }
 }
--- src/main/java/com/dataseek/xe/entity/OauthInfo.java
+++ src/main/java/com/dataseek/xe/entity/OauthInfo.java
@@ -37,6 +37,102 @@ public class OauthInfo {
     private String oauth_consumer_key;
     //oauth_consumer_secret
     private String oauth_consumer_secret;
-    //request
+    //request_token
+    private String request_token;
+    //request_secret
+    private String request_secret;
+    //access_token
+    private String access_token;
+    //access_secret
+    private String access_secret;
+    //更新时间
+    private String update_time;
 
+    public String getAuthorize_url() {
+        return authorize_url;
+    }
+
+    public void setAuthorize_url(String authorize_url) {
+        this.authorize_url = authorize_url;
+    }
+
+    public String getAuthorize_status() {
+        return authorize_status;
+    }
+
+    public void setAuthorize_status(String authorize_status) {
+        this.authorize_status = authorize_status;
+    }
+
+    public String getApp_account() {
+        return app_account;
+    }
+
+    public void setApp_account(String app_account) {
+        this.app_account = app_account;
+    }
+
+    public String getEtsy_account() {
+        return etsy_account;
+    }
+
+    public void setEtsy_account(String etsy_account) {
+        this.etsy_account = etsy_account;
+    }
+
+    public String getOauth_consumer_key() {
+        return oauth_consumer_key;
+    }
+
+    public void setOauth_consumer_key(String oauth_consumer_key) {
+        this.oauth_consumer_key = oauth_consumer_key;
+    }
+
+    public String getOauth_consumer_secret() {
+        return oauth_consumer_secret;
+    }
+
+    public void setOauth_consumer_secret(String oauth_consumer_secret) {
+        this.oauth_consumer_secret = oauth_consumer_secret;
+    }
+
+    public String getRequest_token() {
+        return request_token;
+    }
+
+    public void setRequest_token(String request_token) {
+        this.request_token = request_token;
+    }
+
+    public String getRequest_secret() {
+        return request_secret;
+    }
+
+    public void setRequest_secret(String request_secret) {
+        this.request_secret = request_secret;
+    }
+
+    public String getAccess_token() {
+        return access_token;
+    }
+
+    public void setAccess_token(String access_token) {
+        this.access_token = access_token;
+    }
+
+    public String getAccess_secret() {
+        return access_secret;
+    }
+
+    public void setAccess_secret(String access_secret) {
+        this.access_secret = access_secret;
+    }
+
+    public String getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(String update_time) {
+        this.update_time = update_time;
+    }
 }
