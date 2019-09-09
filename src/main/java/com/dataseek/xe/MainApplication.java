b4422aea48253306071586b709716a48332e50f6
samtao821030 <samtao821030@gmail.com>
2019-09-09 14:49:07 +0800

    增加https支持

--- src/main/java/com/dataseek/xe/MainApplication.java
+++ src/main/java/com/dataseek/xe/MainApplication.java
@@ -1,10 +1,15 @@
 package com.dataseek.xe;
 
 import com.dataseek.xe.config.XeAutoConfig;
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -16,7 +21,25 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @ComponentScan(basePackages = {"com.dataseek"})
 public class MainApplication {
+    //HTTP port
+    @Value("${http.port}")
+    private int httpPort;
+
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(httpPort);
+        return connector;
+    }
 }
+
