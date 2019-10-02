a98c108cf7731e5cf466b0065863f1a1532ecb02
Zhelun Gai <zhelun.gai@tmacomms.com>
2019-10-02 15:16:08 +1000

    elevate privilige

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -jar /var/lib/jenkins/workspace/xe_light/target/xe_light-1.0-SNAPSHOT.jar
+sudo nohup java -jar /var/lib/jenkins/workspace/xe_light/target/xe_light-1.0-SNAPSHOT.jar
